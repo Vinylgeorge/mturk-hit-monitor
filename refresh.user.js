7b22603d789aef86ce2f8fa418ba100e7c5ba16f
Vinylgeorge <cpartha2009@hotmail.com>
2025-09-19 16:24:49 +0530

    Update refresh.user.js

--- refresh.user.js
+++ refresh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MTurk Errors — Auto Continue (robust)
 // @namespace    Violentmonkey Scripts
-// @version      1.3
+// @version      1.4
 // @match        https://worker.mturk.com/errors/*
 // @match        https://www.mturk.com/errors/*
 // @match        https://worker.mturk.com/*
