6f16e5afae4a6c13da93f7700414c83d5068ba06
Vinylgeorge <cpartha2009@hotmail.com>
2026-03-23 11:35:00 +0530

    Update Pay_schedule.user.js

--- Pay_schedule.user.js
+++ Pay_schedule.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AB2soft MTurk Payment Cycle Manager
 // @namespace    AB2soft
-// @version      6.7
+// @version      6.8
 // @description  MTurk payment cycle manager with trigger-lock, corrected boundary logic, forced earnings-page verification, and cycle updates
 // @match        https://worker.mturk.com/earnings*
 // @match        https://worker.mturk.com/payment_schedule*
